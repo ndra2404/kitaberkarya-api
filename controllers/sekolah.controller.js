5925e9f1f0384ba725979932992d60541e706c8a
Indra PC <ndra2404@gmail.com>
2022-01-13 10:38:19 +0700

    qr with logoa

--- controllers/sekolah.controller.js
+++ controllers/sekolah.controller.js
@@ -2,6 +2,7 @@ const { sekolahModel } = require('../models')
 const { response } = require('../utils/')
 const QRCode = require('qrcode');
 
+
 const get = (req, res, next) => {
   sekolahModel
     .get()
@@ -14,7 +15,7 @@ const get = (req, res, next) => {
 }
 const generateQR = async (req, res, next) => {
   let sekolahdetail = await sekolahModel.getById(req.params);
-  var img = await QRCode.toDataURL(JSON.stringify(sekolahdetail[0]));
+  var imageData1 = await QRCode.toDataURL(JSON.stringify(sekolahdetail[0]));
   var base64Data = imageData1.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
   var img = Buffer.from(base64Data, 'base64');
 
